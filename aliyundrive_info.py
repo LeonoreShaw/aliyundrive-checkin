4a88b401590e07628230d2d755aba3f1d6fb7505
Léonore Shaw <leonoreshaw@gmail.com>
2024-04-16 23:36:58 +0800

    Update aliyundrive_info.py

--- aliyundrive_info.py
+++ aliyundrive_info.py
@@ -17,11 +17,11 @@ class AliyundriveInfo:
     def __str__(self) -> str:
         message_all = ''
         if self.success:
-            message_all = f'🎉 阿里云盘 Token 有效！\n' \
-                          f'\n用户：{self.user_name}\n' \
+            message_all = f'\n用户：{self.user_name}\n' \
                           f'签到：本月已签到{self.signin_count}次\n' \
                           f'奖励：{self.reward_notice}\n' \
                           // f'任务：{self.task_notice}\n' \
+                          f'\n 🎉 阿里云盘 Token 有效！\n' \
                           f'/n自动签到成功！\n'
                           
 
